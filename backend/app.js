eae691263c906049487a066f585e6f81b2899be4
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-17 22:41:49 +0500

    test 401 3 iteration

--- backend/app.js
+++ backend/app.js
@@ -15,12 +15,15 @@ mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 
 app.use(cors(
   {
-    origin: ['https://mesto.full-front.nomoredomains.monster',
-      'https://api.mesto.full-front.nomoredomains.monster', 'localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    // origin: ['https://mesto.full-front.nomoredomains.monster',
+    //   'https://api.mesto.full-front.nomoredomains.monster', 'localhost:3000'],
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // credentials: true,
+    // allowedHeaders: ['Content-Type', 'Authorization'],
     // exposedHeaders: ['set-cookie'],
+    origin: '*',
+    credentials: true, // access-control-allow-credentials:true
+    optionSuccessStatus: 200,
   },
 ));
 app.use(cookieParser());
