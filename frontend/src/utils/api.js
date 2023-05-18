06e32a284d87899750e0ce8554909d7a0fd52706
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-18 21:06:21 +0500

    fix headers

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -92,7 +92,7 @@ export default new Api({
   credentials: 'include',
   headers: {
     'Content-Type': 'application/json',
-    // Authorization: `Bearer ${localStorage.getItem("token")}`
+    Authorization: `Bearer ${localStorage.getItem("token")}`
   }
 });
 
